2320c6e975f91a2f2dd5e1c7b61618db15318b2c
Philip Buonomo <philip.buonomo@am.jll.com>
2020-04-14 15:45:32 -0400

    doesn't seem to handle async well.
    channels aren't loading?

--- bot.py
+++ bot.py
@@ -6,11 +6,33 @@ from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
 
 client = discord.Client()
 
 @client.event
 async def on_ready():
-    print(f'{client.user} has connected to Discord!')
+    bunguild = await client.fetch_guilds(limit=10).find(lambda m: m.name == 'Bun')
+
+    print(
+        f'{client.user} is connected to the following guild:\n'
+        f'{bunguild.name}(id: {bunguild.id})'
+    )
+
+    client.get_all_channels()
+
+    print(
+        f'{len(bunguild.text_channels)}'
+        f'{len(bunguild.channels)}'
+    )
+
+
+    for channel in bunguild.text_channels:
+      messages = await channel.history(limit=200).filter(lambda m: m.author.name == 'Avrae').flatten()
+      for message in messages:
+        print(
+            f'{message.content}'
+        )
+      break
 
 client.run(TOKEN)
